67cc5d5489947e75406382355a95b7eb78c4aab2
Mohammed Adel <mohammed28adel@gmail.com>
2026-05-11 11:22:54 +0300

    Fix socket issue.

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,9 +1,12 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', {
-  transports: ['websocket'],
-  autoConnect: true,
-  reconnectionAttempts: 5,
-});
+const socket = io(
+  import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+  {
+    transports: ['websocket'],
+    autoConnect: true,
+    reconnectionAttempts: 5,
+  }
+);
 
 export default socket;
